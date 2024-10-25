522e4fcd2a6f0787acdfc4e9a447be429cecfee0
todd kleber <toddkman11@gmail.com>
2024-10-25 09:56:38 -0700

    Implemented the update email and moved the button to the general to be more intune witht he update name

--- voice_memories/src/components/pages/auth/main/settings/Settings.tsx
+++ voice_memories/src/components/pages/auth/main/settings/Settings.tsx
@@ -476,6 +476,13 @@ export function Settings() {
                           </dt>
                           <dd className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
                             <div className="text-gray-900">{email}</div>
+                            <button
+                                type="button"
+                            onClick={() => setUpdateEmail(true)}
+                            className="font-semibold text-blue-600 hover:text-blue-500"
+                            >
+                            Update Email
+                           </button>
                           </dd>
                         </div>
                     )}
@@ -612,24 +619,6 @@ export function Settings() {
                         </Button>
                       </dd>
                     </div>
-
-                    <div className="pt-6 sm:flex">
-                      <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
-                        Change Email
-                      </dt>
-                      <dd className="mt-1 flex justify-between gap-x-6 sm:flex-none sm:pr-6">
-                        <Button
-                          size="md"
-                          color="red"
-                          className="text-3xl text-white hover:text-gray-200"
-                          onClick={() => setChangePassword(true)}
-                        >
-                          {' '}
-                          Change Email{' '}
-                        </Button>
-                      </dd>
-                    </div>
-
                     <div className="pt-6 sm:flex">
                       <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
                         Delete All My Photos & Data
