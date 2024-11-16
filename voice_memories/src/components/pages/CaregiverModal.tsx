7992a1432f625b03bdc07fef2a67ddf34e208ec9
Carlos Sanchez <cs061258@gmail.com>
2024-11-15 20:48:05 -0700

    Combined the caregiver ui page with the client photo upload UI

--- voice_memories/src/components/pages/CaregiverModal.tsx
+++ voice_memories/src/components/pages/CaregiverModal.tsx
@@ -3,21 +3,6 @@ import {useRouter} from "next/router";
 import { AccountContext } from "../utils/Account";
 import { useContext } from "react";
 
-const { getSession, logout } = useContext(AccountContext);
-
-//A router routes between different pages in the application
-const pageRouter = useRouter();
-
-//Functions
-const logOutUser = () => {
-    logout();
-    //Remove from session Storage
-    sessionStorage.removeItem('FullName');
-    sessionStorage.removeItem('Email');
-    //Route back to the LoginPage
-    pageRouter.push("/AccountLogin");
-}
-
 //Functions that will be needed to retrieve client photos and information
 const getClientImages = () => {
 }
@@ -30,104 +15,109 @@ const uploadVoiceRecording  = () =>{
 const addPhotosToAlbum = () => {
 }
 
-export function CareGiverModel(){
+export function CareGiverModel({pageRouter}){
+    //Functions
+
+    const { getSession, logout } = useContext(AccountContext);
+    const logOutUser = () => {
+        logout();
+        //Remove from session Storage
+        sessionStorage.removeItem('FullName');
+        sessionStorage.removeItem('Email');
+        //Route back to the LoginPage
+        pageRouter.push("/AccountLogin");
+    }
+
     return (
-        <div className = "bg-slate-300 border-2 border-black rounded-none">
+        <div className = "bg-primaryDarkGray border-2 border-black rounded-none h-screen">
             <div className="flex h-screen flex-col items-center">
-                <h1 className="underline bold text-center text-3xl">Managing Content For Person 1</h1>
+                <h1 className="underline font-bold text-center text-2xl">Managing Content For Person 1</h1>
                 <br />
-                <div className="flex flex-row justify-center overflow-x-auto border-2 border-blue-300">
+                <div className="flex flex-row justify-center overflow-x-auto border-2 border-gold">
                     <div className="px-2">
-                        <button className="border-2 border-yellow-400">
+                        <button className="border-2 border-gold">
                             <img src="" alt="Photo uploaded" />
                         </button>
                     </div>
                     <div className="px-2">
-                        <button className="border-2 border-yellow-400">
+                        <button className="border-2 border-gold">
                             <img src="" alt="Photo uploaded" />
                         </button>
                     </div>
                     <div className="px-2">
-                        <button className="border-2 border-yellow-400">
+                        <button className="border-2 border-gold">
                             <img src="" alt="Photo uploaded" />
                         </button>
                     </div>
                     <div className="px-2">
-                        <button className="border-2 border-yellow-400">
+                        <button className="border-2 border-gold">
                             <img src="" alt="Photo uploaded" />
                         </button>
                     </div>
                     <div className="px-2">
-                        <button className="border-2 border-yellow-400">
+                        <button className="border-2 border-gold">
                             <img src="" alt="Photo uploaded" />
                         </button>
                     </div>
                     <div className="px-2">
-                        <button className="border-2 border-yellow-400">
+                        <button className="border-2 border-gold">
                             <img src="" alt="Photo uploaded" />
                         </button>
                     </div>
                 </div>
 
                 <div className="flex flex-col items-center">
-                    <h1 className="bold text-xl">Choose an Album to upload the Photos too:</h1>
-                    <div className="flex max-h-52 w-80 flex-col items-center overflow-y-auto rounded-md border-2 border-gray-400">
-                        <button className="my-1 w-1/2 rounded-md border-2 border-yellow-400 bg-black p-2 text-white hover:bg-slate-500">
+                    <h1 className="font-bold text-xl">Choose an Album to upload the Photos too:</h1>
+                    <div className="flex max-h-52 w-80 flex-col items-center overflow-y-auto rounded-md border-2 border-black">
+                        <button className="my-1 w-1/2 rounded-md border-2 border-gold bg-black p-2 text-white hover:bg-slate-500">
                             Christmas 04
                         </button>
-                        <button className="my-1 w-1/2 rounded-md border-2 border-yellow-400 bg-black p-2 text-white hover:bg-slate-500">
+                        <button className="my-1 w-1/2 rounded-md border-2 border-gold bg-black p-2 text-white hover:bg-slate-500">
                             New Years 21
                         </button>
-                        <button className="my-1 w-1/2 rounded-md border-2 border-yellow-400 bg-black p-2 text-white hover:bg-slate-500">
+                        <button className="my-1 w-1/2 rounded-md border-2 border-gold bg-black p-2 text-white hover:bg-slate-500">
                             Thanksgiving 87
                         </button>
-                        <button className="my-1 w-1/2 rounded-md border-2 border-yellow-400 bg-black p-2 text-white hover:bg-slate-500">
+                        <button className="my-1 w-1/2 rounded-md border-2 border-gold bg-black p-2 text-white hover:bg-slate-500">
                             Birthday Party
                         </button>
-                        <button className="my-1 w-1/2 rounded-md border-2 border-yellow-400 bg-black p-2 text-white hover:bg-slate-500">
+                        <button className="my-1 w-1/2 rounded-md border-2 border-gold bg-black p-2 text-white hover:bg-slate-500">
                             Birthday Party 2002
                         </button>
-                        <button className="my-1 w-1/2 rounded-md border-2 border-yellow-400 bg-black p-2 text-white hover:bg-slate-500">
+                        <button className="my-1 w-1/2 rounded-md border-2 border-gold bg-black p-2 text-white hover:bg-slate-500">
                             Halloween
                         </button>
-                        <button className="my-1 w-1/2 rounded-md border-2 border-yellow-400 bg-black p-2 text-white hover:bg-slate-500">
+                        <button className="my-1 w-1/2 rounded-md border-2 border-gold bg-black p-2 text-white hover:bg-slate-500">
                             Thanksgiving 87
                         </button>
                     </div>
                 </div>
                 <div className="mt-12 flex h-1/4 flex-col items-center">
                     <div className="mb-1">
-                        <button className="w-52 rounded-md border-2 border-yellow-400 bg-black py-2 text-white hover:bg-slate-500">
+                        <button className="w-52 mr-2 rounded-md border-2 border-gold bg-black py-2 text-white hover:bg-slate-500">
                             Upload Photos
                         </button>
-                        <button className="w-52 rounded-md border-2 border-yellow-400 bg-black py-2 text-white hover:bg-slate-500">
+                        <button className="w-52 rounded-md border-2 border-gold bg-black py-2 text-white hover:bg-slate-500">
                             Upload PDF Documents
                         </button>
                     </div>
-                    <button className="w-52 rounded-md border-2 border-yellow-400 bg-black py-2 text-white hover:bg-slate-500">Upload Voice Recording</button>
+                    <button className="w-52 rounded-md border-2 border-gold bg-black py-2 text-white hover:bg-slate-500">Upload Voice Recording</button>
                 </div>
 
-                <div className = "flex flex-col items-center">
-                    <button className="px-3 rounded-md border-2 border-yellow-400 bg-black text-3xl text-white hover:bg-slate-400 hover:text-black"
-                        onClick = {() => pageRouter.push("./..")}>
-                        Go Back to Clients
-                    </button>
-                </div>
-
-                <div className="flex justify-center pb-10 text-center pt-5">
-                    <div className="flex justify-between pr-4">
-                        <button className="rounded-md border-2 border-yellow-400 bg-black text-3xl text-white hover:bg-slate-400 hover:text-black"
-                            onClick = {() => pageRouter.push("/auth/main")}>
+                <div className = "flex justify-center text-center pb-10 ">
+                    <div className="justify-between flex pr-4">
+                        <button className="text-3xl w-52 h-16 rounded-md border-2 border-gold bg-black py-2 text-white hover:bg-slate-500" 
+                            onClick={() => pageRouter.push('/auth/main')}>
                             Home
                         </button>
                     </div>
-                    <div className="flex justify-between pr-4">
-                        <button className="hover:text-blackrounded-md border-2 border-yellow-400 bg-black text-3xl text-white hover:bg-slate-400"
-                            onClick = {logOutUser}>
-                            Logout
-                        </button>
-                    </div>
+                <div className="justify-between flex pr-4">
+                    <button className="text-3xl w-52 h-16 rounded-md border-2 border-gold bg-black py-2 text-white hover:bg-slate-500"
+                        onClick={logOutUser}>
+                        Logout
+                    </button>
                 </div>
+            </div>
             </div>
         </div>
 
--- voice_memories/src/components/pages/auth/main/viewClients/ViewClients.tsx
+++ voice_memories/src/components/pages/auth/main/viewClients/ViewClients.tsx
@@ -5,6 +5,8 @@ import { Button } from '@/components/primitives/Button'
 import { TileSpinner } from '@/components/primitives/TileSpinner.tsx/TileSpinner'
 import { AuthLayout } from '@/components/pages/AuthLayout'
 import { useRouter } from 'next/navigation'
+import { CareGiverModel } from '@/components/pages/CaregiverModal'
+
 //Logging out the user through AWS
 import { AccountContext } from '@/components/utils/Account'
 
@@ -17,6 +19,17 @@ export function ViewClients(){
     const pageRouter = useRouter();
     const {logout } = useContext(AccountContext)
 
+    const [isUploading, setIsUploading] = useState(false);
+
+    function setLoading(enable){
+        //Prevent doing a re-render of the Client Model Component
+        if(!isUploading && !enable){
+            return;
+        }
+        setIsUploading(enable);
+    }
+
+
     const logOutUser = () => {
         logout();
         //Remove the user name and email from the current session
@@ -26,8 +39,9 @@ export function ViewClients(){
         pageRouter.push('/AccountLogin');
     }
 
+    //If images begin to push the client component modal change overflow-y-hidden to the y-scroll instead
     return(
-        <div className = "flex h-screen flex-col"
+        <div className = "flex h-screen flex-col overflow-y-hidden"
         style={{
             backgroundImage: "url('https://wallpapercave.com/wp/wp8522376.jpg')",
             backgroundSize: 'cover',
@@ -39,54 +53,64 @@ export function ViewClients(){
                 <div className='flex flex-col w-min border-4 border-primaryBlue rounded-md overflow-y-auto max-h-96 bg-primaryDarkGray'> 
                     <h1 className='text-xl text-center font-bold'>Your Clients:</h1>
                     <button className='w-52 rounded-md border-2 text-center border-gold 
-                    hover:bg-primaryGray hover:text-black bg-black text-white'>Person 1 Name</button>
+                    hover:bg-primaryGray hover:text-black bg-black text-white' onClick={() => setLoading(false)} >Person 1 Name</button>
                     <button className='my-3 w-52 rounded-md border-2 text-center border-gold 
-                    hover:bg-primaryGray hover:text-black bg-black text-white'>Person 2 Name</button>
+                    hover:bg-primaryGray hover:text-black bg-black text-white' onClick={() => setLoading(false)}>Person 2 Name</button>
                     <button className='w-52 rounded-md border-2 text-center border-gold 
-                    hover:bg-primaryGray hover:text-black bg-black text-white'>Person 3 Name</button>
+                    hover:bg-primaryGray hover:text-black bg-black text-white' onClick={() => setLoading(false)}>Person 3 Name</button>
                     <button className='my-3 w-52 rounded-md border-2 text-center border-gold 
-                    hover:bg-primaryGray hover:text-black bg-black text-white'>Person 4 Name</button>
+                    hover:bg-primaryGray hover:text-black bg-black text-white' onClick={() => setLoading(false)}>Person 4 Name</button>
                     <button className='w-52 rounded-md border-2 text-center border-gold 
-                    hover:bg-primaryGray hover:text-black bg-black text-white'>Person 5 Name</button>
+                    hover:bg-primaryGray hover:text-black bg-black text-white' onClick={() => setLoading(false)}>Person 5 Name</button>
                     <button className='my-3 w-52 rounded-md border-2 text-center border-gold
-                    hover:bg-primaryGray hover:text-black bg-black text-white'>Person 6 Name</button>
+                    hover:bg-primaryGray hover:text-black bg-black text-white' onClick={() => setLoading(false)}>Person 6 Name</button>
                     <button className='w-52 rounded-md border-2 text-center border-gold 
-                    hover:bg-primaryGray hover:text-black bg-black text-white'>Person 7 Name</button>
+                    hover:bg-primaryGray hover:text-black bg-black text-white' onClick={() => setLoading(false)}>Person 7 Name</button>
                     <button className='my-3 w-52 rounded-md border-2 text-center border-gold
-                    hover:bg-primaryGray hover:text-black bg-black text-white'>Placeholder Name</button>
+                    hover:bg-primaryGray hover:text-black bg-black text-white' onClick={() => setLoading(false)}>Placeholder Name</button>
 
                     <button className='w-52 rounded-md border-2 text-center border-gold
-                    hover:bg-primaryGray hover:text-black bg-black text-white'>Person 8 Name</button>
+                    hover:bg-primaryGray hover:text-black bg-black text-white' onClick={() => setLoading(false)}>Person 8 Name</button>
                     <button className='my-3 w-52 rounded-md border-2 text-center border-gold
-                    hover:bg-primaryGray hover:text-black bg-black text-white'>Person 9 Name</button>
+                    hover:bg-primaryGray hover:text-black bg-black text-white' onClick={() => setLoading(false)}>Person 9 Name</button>
                     <button className='w-52 rounded-md border-2 text-center border-gold
-                    hover:bg-primaryGray hover:text-black bg-black text-white'>Person 10 Name</button>  
+                    hover:bg-primaryGray hover:text-black bg-black text-white' onClick={() => setLoading(false)}>Person 10 Name</button>  
                 </div>
 
                 <div className='flex flex-col flex-grow max-h-96 bg-primaryBlue bg-opacity-85 rounded-md'>
-                    <h1 className='text-center text-xl underline font-bold'>Manage Settings for Person 1</h1>
-                    <button className="mt-5 text-white rounded-md h-1/6 w-1/6 mx-auto hover:bg-primaryGray 
-                    hover:text-black border-2 border-gold bg-black">Upload Photos/Audio</button>
+                    {!isUploading ? 
+                        <>
+                            <h1 className='text-center text-xl underline font-bold'>Manage Settings for Person 1</h1>
+                            <button className="mt-5 text-white rounded-md h-1/6 w-1/6 mx-auto hover:bg-primaryGray 
+                            hover:text-black border-2 border-gold bg-black"
+                            onClick={() => setLoading(true)}
+                            >Upload Photos/Audio</button>
+                        </>
+                    :
+                        <CareGiverModel pageRouter={pageRouter}></CareGiverModel>
+                    }
                 </div>
             </div>
 
-            <div className = "flex justify-center text-center pb-10 ">
-                <div className="justify-between flex pr-4">
-                    <Button size = "xl" 
-                            className="text-3xl hover:text-primaryGray text-white border-2 border-gold "
-                            onClick={() => pageRouter.push('/auth/main')} //point to main page with all items
-                    >Home
-                    </Button>
-                </div>
+            {!isUploading &&
+                <div className = "flex justify-center text-center pb-10 ">
+                    <div className="justify-between flex pr-4">
+                        <Button size = "xl" 
+                                className="text-3xl hover:text-primaryGray text-white border-2 border-gold "
+                                onClick={() => pageRouter.push('/auth/main')} //point to main page with all items
+                        >Home
+                        </Button>
+                    </div>
 
-                <div className="justify-between flex pr-4">
-                    <Button size = "xl" 
-                            className="text-3xl hover:text-primaryGray text-white border-2 border-gold "
-                            onClick={logOutUser} //page.tsx Logout user here
-                    >Logout
-                    </Button>
+                    <div className="justify-between flex pr-4">
+                        <Button size = "xl" 
+                                className="text-3xl hover:text-primaryGray text-white border-2 border-gold "
+                                onClick={logOutUser} //page.tsx Logout user here
+                        >Logout
+                        </Button>
+                    </div>
                 </div>
-            </div>
+            }
         </div>
     )
    
